d1da89b4c4bfa028013f101bae1f4c63ca1e532a
Alejandro Hernandez <alejandrojaez@outlook.com>
2023-07-26 17:54:51 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+
+Ajusta las variables en el archivo 
+<code>
+.env</code>
+
+instalar poetry con el comando:
+<code>
+pip install poetry
+</code>
+
+desde la consola ubicarse en la carpeta de este proyecto y ejecutar la instalacion de las depencias:
+<code>
+poetry install
+</code>
+
+ejecutar el bot usando:
+<code>
+poetry run python main.py
+</code>
---
+++ main.py
@@ -0,0 +1,65 @@
+import logging
+import pathlib
+
+from telegram import Update, Location
+from telegram.ext import (
+    ApplicationBuilder,
+    ContextTypes,
+    CommandHandler,
+    MessageHandler,
+    filters,
+)
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
+    )
+
+
+async def downloader(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not isinstance(update.message.effective_attachment, Location):
+        if isinstance(update.message.effective_attachment, tuple):
+            attachment = await context.bot.get_file(
+                update.message.effective_attachment[-1].file_id
+            )
+            file_extension = attachment.file_path.rsplit(".", 1)[-1]
+            filename = attachment.file_unique_id + "." + file_extension
+        else:
+            attachment = await context.bot.get_file(
+                update.message.effective_attachment.file_id
+            )
+            filename = update.message.effective_attachment.file_name
+        file = await attachment.download_to_drive(
+            custom_path="{0}/{1}".format(config["TOKEN"], filename)
+        )
+        if isinstance(file, pathlib.WindowsPath):
+            send = await context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text="Archivo {} recibido!".format(filename),
+                reply_to_message_id=update.message.id,
+            )
+
+        else:
+            send = await context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text="Error en el almacenamiento de archivo!",
+            )
+        print(file)
+
+
+if __name__ == "__main__":
+    application = ApplicationBuilder().token(config["TOKEN"]).build()
+
+    start_handler = CommandHandler("start", start)
+    application.add_handler(start_handler)
+    application.add_handler(MessageHandler(filters.ATTACHMENT, downloader))
+
+    application.run_polling()
